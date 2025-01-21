a247dcc094877ffed78b55069db58eb5e4960fb9
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-01-20 19:38:46 -0600

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,12 @@
+
+console.log("*****************************"); 
+console.log("*                             *");
+console.log("*         KYLIE               *");
+console.log("*                             *");
+console.log("*******************************");
+console.log("enter your namee below:")
+console.log("Kylie Nguyen")
+console.log("₍⑅ᐢ..ᐢ₎! ")
+console.log("heres a cute bunny!")
+
+
