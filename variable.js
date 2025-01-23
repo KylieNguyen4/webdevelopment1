a84c5eac6f7bec4571fe573732e2763412d22ec5
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-01-22 19:41:59 -0600

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,8 @@
+let favsnack = "pizza"; //variales is created 
+console.log(favsnack); //output
+
+let name = 'Kylie';
+let age = 10;
+let bookReads = 10;
+console.log(name);
+console.log("My super Hero is "+superhero);
