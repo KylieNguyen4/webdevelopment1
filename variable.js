87ce3a26575f8094b05dbc9368545487657f934f
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-01-24 19:45:16 -0600

    Add files via upload

--- variable.js
+++ variable.js
@@ -5,4 +5,22 @@ let name = 'Kylie';
 let age = 10;
 let bookReads = 10;
 console.log(name);
-console.log("My super Hero is "+superhero);
+//creating varibles
+let firstName = "Kylie";
+let grade = 5;
+let isHappy = true;
+//logging to the console
+console.log(firstName, grade, isHappy)
+console.log("The girl name is "+firstName + ". She is in grade "+grade +" . She is happy? "+isHappy);
+
+//TASK: create variable for classroom(student,)
+//airport, hospital and log to console.
+//Select 1 scenarios, create varibles,
+// and display.
+//simple maths
+let number1 = 20;
+let number2 = 10;
+console.log("sum,: , number1 + number2");
+console.log("Diffrence: ", number1 - number2);
+console.log("Product: ", number1 * number2);
+console.log("Divide: ", number1 / number2);
