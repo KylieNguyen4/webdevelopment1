5eda073c8063e0fca706e729cc9397e713583a00
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-01-31 19:35:55 -0600

    Add files via upload

---
+++ cat.js
@@ -0,0 +1,23 @@
+console.log("   ^_^");
+console.log(" (≽••≼) ");
+console.log(" (  V  )");
+console.log("Heres a cute cat");
+console.log("The cats name is Fluffy");
+
+console.log(" ^_^");
+console.log("(o.o)");
+console.log("( V )");
+
+let name="Kylie";
+let age=10;
+let live="USA";
+let favfood="Any spicy food";
+console.log(name,age,live,favfood);
+console.log("My name is"+name+ "I am"+age+ "I live in the"+live+ "I like"+favfood);
+//simple math
+let number1 = 20;
+let number2 = 10;
+console.log("Sum: ", number1 + number2);
+console.log("Diffrence: ", number1 - number2);
+console.log("Product: ", number1 * number2);
+console.log("Divide: ", number1 / number2);
