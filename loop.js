dd734d70d1fa2584fa4fcc0bf0c34f34fe2e1af4
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-03-14 19:48:28 -0500

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,12 @@
+console.log("Hello WORLD!")
+
+for (counter = 1; counter<=5; counter++){
+    console.log("Test1", counter)
+}
+
+
+console.log("=My fav")
+
+for (counter = 1; counter<=10; counter++){
+    console.log("I love coding", counter)
+}
