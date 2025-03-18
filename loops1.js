f40b5fdf5fe5d794647f8b541498a5918b0b1f1a
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-03-17 19:28:22 -0500

    Add files via upload

---
+++ loops1.js
@@ -0,0 +1,10 @@
+console.log("=Here are number 1-5")
+
+for (counter = 2; counter<=5; counter++){
+    console.log( counter)
+}
+console.log("=My fav")
+
+for (counter = 1; counter<=3; counter++){
+    console.log("Hello", counter)
+}
