9c60c01edebac9832432ce6812e754bfa18b6f4c
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-03-21 19:34:53 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,14 @@
+let candies = ["Vanilla", "Strawberry", "Chocolate", "Mint"]
+
+console.log(candies[3]);
+console.log(candies[2]);
+console.log(candies[1]);
+console.log(candies[0]);
+let age =[7, 8, 10, 15]
+console.log(candies[7]);
+
+
+let colors =["red", "blue", "green", "orange"]
+console.log(colors[1]);
+console.log(colors[2]);
+console.log(colors[3]);
