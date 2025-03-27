927ee7faf7e7d6185fd5a7771716bbe6a6176409
KylieNguyen4 <141271854+KylieNguyen4@users.noreply.github.com>
2025-03-26 19:37:06 -0500

    Add files via upload

---
+++ puspop.js
@@ -0,0 +1,20 @@
+//push method is used for adding items in the array
+let fruit = ["apple", "banana"]
+console.log(fruit)
+
+fruit.push("mango");
+console.log(fruit);
+
+fruit.push("orange");
+console.log(fruit);
+
+//pop is used for removed items from the array
+fruit.pop()
+fruit.pop()
+console.log(fruit);
+
+let color = ["red", "blue", "pink"]
+console.log(color);
+
+let removeColor = color.pop()
+console.log(removeColor);
